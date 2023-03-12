ce3a5c2f0a28a7342a2b3ccb4425714688d8f8ec
aduarte-manusvr <andreduarte@manus-vr.com>
2023-03-12 09:06:16 +0100

    Second commit

--- src/script.js
+++ src/script.js
@@ -36,7 +36,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Welcome live Three.js',
             {
                 font: font,
                 size: 0.5,
